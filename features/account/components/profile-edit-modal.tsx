0de22e17f173ca5f78c5c5dcc90fc5ee408bffd1
前田正勝 <katsukatsu.masa.0522@gmail.com>
2024-05-18 13:39:54 +0900

    プロフィール編集モーダルmock作成

---
+++ features/account/components/profile-edit-modal.tsx
@@ -0,0 +1,89 @@
+import RoundedWhiteCamera from '@/assets/round_white_camera.svg';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Typography } from '@/components/ui/typography';
+import { PencilRuler } from 'lucide-react';
+import Image from 'next/image';
+
+export default async function ProfileEditModal() {
+  return (
+    <Dialog>
+      <DialogDescription>
+        <DialogTrigger asChild>
+          <Button
+            type="button"
+            className="ml-[24px] flex w-full items-center justify-center gap-2 border border-bibinBlue-100 bg-inherit"
+          >
+            <PencilRuler className="h-6 w-6" color="rgb(81 183 255 / var(--tw-text-opacity)" />
+            <Typography as="bold" element="p" className="text-bibinBlue-100">
+              編集
+            </Typography>
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="dialog-center flex flex-col items-center justify-center sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>プロフィール編集</DialogTitle>
+          </DialogHeader>
+          <div className="relative flex">
+            <Image
+              src={'/yamada_yuka_demo.png'}
+              width={100}
+              height={100}
+              className="rounded-[100px]"
+              alt={''}
+            />
+            <RoundedWhiteCamera className="absolute bottom-0 right-0" />
+          </div>
+          <div className="w-full">
+            <Typography as="bold" element="p" className="mb-[8px] text-[14px] text-black-90">
+              名前
+            </Typography>
+            <Input defaultValue="yamada_yuka183" />
+          </div>
+          <div className="w-full">
+            <Typography as="bold" element="p" className="mb-[8px] text-[14px] text-black-90">
+              性別
+            </Typography>
+            <RadioGroup defaultValue="female" className="mt-[4px] flex">
+              <div className="mr-[40px] flex items-center">
+                <RadioGroupItem value="female" id="r1" className=" focus" />
+                <Typography as="xSmall" element="p" className="ml-[8px] text-[14px] text-black-90">
+                  女性
+                </Typography>
+              </div>
+              <div className="mr-[40px] flex items-center">
+                <RadioGroupItem value="male" id="r2" />
+                <Typography as="xSmall" element="p" className="ml-[8px] text-[14px] text-black-90">
+                  男性
+                </Typography>
+              </div>
+              <div className="mr-[40px] flex items-center">
+                <RadioGroupItem value="other" id="r3" />
+                <Typography as="xSmall" element="p" className="ml-[8px] text-[14px] text-black-90">
+                  その他
+                </Typography>
+              </div>
+            </RadioGroup>
+          </div>
+          <div className="w-full">
+            <DialogClose asChild>
+              <Button type="submit" size="lg" variant="lg" className="w-full">
+                保存
+              </Button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </DialogDescription>
+    </Dialog>
+  );
+}
