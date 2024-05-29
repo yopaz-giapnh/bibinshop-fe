df15820d5a8dd6ac77046f047e4f7d0abe55536d
jin-331 <luckina.331@gmail.com>
2024-05-29 17:56:55 +0900

    ソートボタンを修正

--- components/button/sort-button.tsx
+++ components/button/sort-button.tsx
@@ -1,13 +1,32 @@
-import { ChevronDown } from 'lucide-react';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
 
+// TODO: API でなんのソートができるか確認
 export function SortButton() {
   return (
-    <button className="relative flex h-[32px] w-[240px] items-center justify-center rounded-full border border-[#51B7FF] py-2.5 pl-5 text-[#51B7FF]">
-      <span className="text-sm font-medium leading-[20px] tracking-[0.03em]">
-        {/* TODO: ソート内容確認 */}
-        並べ替え: ランキング順
-      </span>
-      <ChevronDown className="ml-2 h-6 w-6 transform" />
-    </button>
+    <Select defaultValue="ranking">
+      <SelectTrigger className="w-[180px] rounded-full border border-bibinBlue-100 text-bibinBlue-100 focus:outline-none focus:ring-0">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value="ranking" className="w-[180px] text-bibinBlue-100">
+            ランキング順
+          </SelectItem>
+          <SelectItem value="review" className="w-[180px] text-bibinBlue-100">
+            ベストセラー
+          </SelectItem>
+          <SelectItem value="new" className="w-[180px] text-bibinBlue-100">
+            新着
+          </SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 }
