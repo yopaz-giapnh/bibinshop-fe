69114f20324e35cddcc04c30ea807c6227beedfd
koooosuke <maeda.kosuke2507.git@gmail.com>
2024-06-13 20:50:41 +0900

    Add import statement for client in filterForm component

--- features/vendor/components/filterForm.tsx
+++ features/vendor/components/filterForm.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { Checkbox } from '@/components/ui/checkbox';
 import { Typography } from '@/components/ui/typography';
 import { getTaxons } from '@/features/taxon/actions';
