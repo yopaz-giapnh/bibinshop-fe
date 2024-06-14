de71a38ac3f8752d2da6c01e9876e28f7d471080
jin-331 <luckina.331@gmail.com>
2024-06-14 19:36:19 +0900

    ページングした時に他のクエリが消えるバグを修正

--- features/pagination/components/pagination.tsx
+++ features/pagination/components/pagination.tsx
@@ -15,30 +15,36 @@ type Props = {
 const MAX_PAGES_TO_SHOW = 3; // 中央に表示するページリンクの最大数
 
 export default function CustomPagination({ totalPages }: Props) {
-  const pathName = usePathname();
+  const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
   const startPage = Math.max(currentPage - Math.floor(MAX_PAGES_TO_SHOW / 2), 1);
   const endPageToShow = Math.min(startPage + MAX_PAGES_TO_SHOW - 1, totalPages);
 
+  const getPageLink = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', page.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+
   return (
     <Pagination className="mt-[24px]">
       <PaginationContent>
         <PaginationItem>
-          <PaginationLink href={`${pathName}?page=${currentPage - 1}`}>
+          <PaginationLink href={getPageLink(currentPage - 1)}>
             <ChevronLeft className="h-4 w-4" />
           </PaginationLink>
         </PaginationItem>
         {startPage > 1 && (
           <PaginationItem>
-            <PaginationLink href={`${pathName}?page=1`}>1</PaginationLink>
+            <PaginationLink href={getPageLink(1)}>1</PaginationLink>
           </PaginationItem>
         )}
         {startPage > 2 && <PaginationItem>...</PaginationItem>}
         {Array.from({ length: endPageToShow - startPage + 1 }, (_, i) => startPage + i).map(
           (page) => (
             <PaginationItem key={page}>
-              <PaginationLink href={`${pathName}?page=${page}`} isActive={page === currentPage}>
+              <PaginationLink href={getPageLink(page)} isActive={page === currentPage}>
                 {page}
               </PaginationLink>
             </PaginationItem>
@@ -47,11 +53,11 @@ export default function CustomPagination({ totalPages }: Props) {
         {endPageToShow < totalPages - 1 && <PaginationItem>...</PaginationItem>}
         {endPageToShow < totalPages && (
           <PaginationItem>
-            <PaginationLink href={`${pathName}?page=${totalPages}`}>{totalPages}</PaginationLink>
+            <PaginationLink href={getPageLink(totalPages)}>{totalPages}</PaginationLink>
           </PaginationItem>
         )}
         <PaginationItem>
-          <PaginationLink href={`${pathName}?page=${currentPage + 1}`}>
+          <PaginationLink href={getPageLink(currentPage + 1)}>
             <ChevronRight className="h-4 w-4" />
           </PaginationLink>
         </PaginationItem>
