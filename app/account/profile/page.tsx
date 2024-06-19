e6be557ab59a679ad7c44c4f66e01099877408ae
koooosuke <maeda.kosuke2507.git@gmail.com>
2024-06-20 07:41:41 +0900

    Add ProfileReviewList component to profile page

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -1,4 +1,5 @@
 import ProfileDetail from '@/features/account/profile/components/profile-detail';
+import ProfileReviewList from '@/features/account/profile/components/profile-review-list';
 import { Suspense } from 'react';
 
 /**
@@ -10,6 +11,9 @@ export default async function Page() {
     <div className="mx-auto flex h-screen w-full flex-col justify-center bg-paleFrostBlue p-[24px]">
       <Suspense fallback={<div>Loading...</div>}>
         <ProfileDetail />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProfileReviewList />
       </Suspense>
     </div>
   );
