86d900bb21560ac6cc181db82d5aae54259fa265
koooosuke <maeda.kosuke2507.git@gmail.com>
2024-06-20 11:55:49 +0900

    Update social media icons to include id prop.- Add id prop to FacebookLogo, InstagramLogo, and XLogo

--- components/icons/facebook-logo.tsx
+++ components/icons/facebook-logo.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-export function FacebookLogo(props: React.ComponentProps<'svg'>) {
+export function FacebookLogo(props: React.ComponentProps<'svg'> & { id: string }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" {...props}>
       <path
-        fill="url(#facebook)"
+        fill={`url(#facebook-${props.id})`}
         fillRule="evenodd"
         d="M12 24c6.627 0 12-5.373 12-12S18.627 0 12 0 0 5.373 0 12s5.373 12 12 12Zm1.78-19 1.919.003c.166 0 .301.128.301.285v2.138a.294.294 0 0 1-.302.285h-1.292c-.891 0-1.054.33-1.054.976v1.347h2.256c.08 0 .157.03.214.084a.278.278 0 0 1 .088.202v2.301a.294.294 0 0 1-.303.286h-2.255v5.808a.294.294 0 0 1-.302.285h-2.517a.294.294 0 0 1-.302-.285v-5.808h-1.93A.294.294 0 0 1 8 12.62V10.32c0-.158.135-.286.302-.286h1.929V8.445C10.231 6.352 11.624 5 13.78 5Z"
         clipRule="evenodd"
       />
       <defs>
         <linearGradient
-          id="facebook"
+          id={`facebook-${props.id}`}
           x1={0}
           x2={24}
           y1={24}
--- components/layout/footer/copyright.tsx
+++ components/layout/footer/copyright.tsx
@@ -1,17 +1,12 @@
-'use client';
-
 import { FacebookLogo } from '@/components/icons/facebook-logo';
 import { InstagramLogo } from '@/components/icons/instagram-logo';
 import { Logo } from '@/components/icons/logo';
 import { XLogo } from '@/components/icons/x-logo';
 import { Typography } from '@/components/ui/typography';
 import Link from 'next/link';
-import useMedia from 'use-media';
 
 export function Copyright() {
-  const isPc = useMedia({ minWidth: '768px' });
-
-  const SocialLinks = () => (
+  const SocialLinks = ({ id }: { id: string }) => (
     <>
       {/* TODO: Facebookアカウント */}
       <Link
@@ -20,7 +15,7 @@ export function Copyright() {
         rel="noopener noreferrer"
         passHref
       >
-        <FacebookLogo />
+        <FacebookLogo id={id} />
       </Link>
       {/* TODO: Xアカウント */}
       <Link
@@ -29,7 +24,7 @@ export function Copyright() {
         rel="noopener noreferrer"
         passHref
       >
-        <XLogo />
+        <XLogo id={id} />
       </Link>
       {/* TODO: Instagramアカウント */}
       <Link
@@ -38,35 +33,27 @@ export function Copyright() {
         rel="noopener noreferrer"
         passHref
       >
-        <InstagramLogo />
+        <InstagramLogo id={id} />
       </Link>
     </>
   );
 
   return (
     <div className="flex flex-col items-center justify-center md:flex-row md:justify-between">
-      <div className={`items-center ${isPc ? 'md:flex' : 'flex flex-col justify-center'}`}>
+      <div className="flex flex-col items-center justify-center md:flex">
         <Logo />
-        {isPc ? (
-          <Typography as="caption" element="p" className="ml-4">
+        <>
+          <div className="mt-[16px] flex gap-5 md:hidden">
+            <SocialLinks id="sp" />
+          </div>
+          <Typography as="caption" element="p" className="mt-[16px]">
             Copyright ⓒ bibin co.,Ltd ALL RIGHTS RESERVED.
           </Typography>
-        ) : (
-          <>
-            <div className="mt-[16px] flex gap-5">
-              <SocialLinks />
-            </div>
-            <Typography as="caption" element="p" className="mt-[16px]">
-              Copyright ⓒ bibin co.,Ltd ALL RIGHTS RESERVED.
-            </Typography>
-          </>
-        )}
+        </>
       </div>
-      {isPc && (
-        <div className="flex gap-5">
-          <SocialLinks />
-        </div>
-      )}
+      <div className="hidden gap-5 md:flex">
+        <SocialLinks id="pc" />
+      </div>
     </div>
   );
 }
