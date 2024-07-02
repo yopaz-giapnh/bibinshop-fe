6cc50b2d5d84a11ea7aebc09b4ae0125f1f0284d
koooosuke <37116394+koooosuke@users.noreply.github.com>
2024-07-03 00:51:51 +0900

    [暫定対応] Next-Authのリフレッシュトークン処理を廃止 (#141)
    * Update authConfig to handle account information in jwt function
    
    * Add session maxAge and handle 401 error in api-client req

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,4 @@
 import type { NextAuthConfig } from 'next-auth';
-import { JWT } from 'next-auth/jwt';
-import { apiClient } from './config/api-client';
-import { calculateAccessTokenExpires } from './features/auth/utils';
 import { isNumber } from './utils/isNumber';
 import { isString } from './utils/string';
 
@@ -13,25 +10,22 @@ export const authConfig = {
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
     // while this file is also used in non-Node.js environments
   ],
+  session: {
+    maxAge: 2592000 // 30 days
+  },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       console.log('nextUrl', nextUrl);
       return isLoggedIn;
     },
-    async jwt({ token, user }) {
+    async jwt({ token, user, account }) {
       // 初回ログイン時
-      if (user) {
+      if (user && account) {
         return { ...token, ...user };
       }
 
-      // トークン有効期限内
-      if (isNumber(token.accessTokenExpires) && Date.now() < token.accessTokenExpires) {
-        return token;
-      }
-
-      // トークン有効期限切れ
-      return refreshAccessToken(token);
+      return token;
     },
     async session({ session, token }) {
       if (
@@ -48,37 +42,3 @@ export const authConfig = {
     }
   }
 } satisfies NextAuthConfig;
-
-const refreshAccessToken = async (token: JWT) => {
-  try {
-    if (!isString(token.refreshToken)) {
-      throw new Error('Missing refresh token');
-    }
-
-    const refreshResponse = await apiClient.POST('/spree_oauth/token', {
-      body: {
-        grant_type: 'refresh_token',
-        refresh_token: token.refreshToken
-      }
-    });
-
-    if (refreshResponse.error) {
-      throw new Error('Failed to refresh access token');
-    }
-
-    return {
-      ...token,
-      accessToken: refreshResponse.data?.access_token,
-      refreshToken: refreshResponse.data?.refresh_token,
-      accessTokenExpires: calculateAccessTokenExpires({
-        createdAt: refreshResponse.data?.created_at,
-        expiresIn: refreshResponse.data?.expires_in
-      })
-    };
-  } catch (error) {
-    return {
-      ...token,
-      error
-    };
-  }
-};
