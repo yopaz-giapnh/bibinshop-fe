6752cb2ed49f64e29c8a861feca45ebbdd79495b
前田正勝 <katsukatsu.masa.0522@gmail.com>
2024-07-02 16:34:17 +0900

    [SP]版のプロフィールがPCに出てしまっていたのを修正

--- app/account/sp-profile/page.tsx
+++ app/account/sp-profile/page.tsx
@@ -9,7 +9,7 @@ import { Suspense } from 'react';
  */
 export default async function Page() {
   return (
-    <div className="mx-auto flex w-full flex-col justify-center bg-paleFrostBlue p-[16px]">
+    <div className="mx-auto flex w-full flex-col justify-center bg-paleFrostBlue p-[16px] md:hidden">
       <Suspense fallback={<LoadingSpinner />}>
         <ProfileDetail />
       </Suspense>
