5d885af467141b2236c058c43cd69f936005fa0d
Tarek <ta.aoukar@gmail.com>
2024-07-03 15:04:33 +0900

    sp-search implementation (#131)
    * sp-search implementation
    
    * fix header md styles
    
    * fix missing key in div
    
    * fix missing callback dependencies
    
    * fix prettier issues
    
    * fix search header for iPhone

--- components/layout/navbar/header.tsx
+++ components/layout/navbar/header.tsx
@@ -15,15 +15,15 @@ type Props = Pick<ComponentProps<typeof AccountMenu>, 'isSignedIn'> & {
 
 export function Header({ isSignedIn, getTaxons }: Props) {
   return (
-    <div className="z-50 flex h-[72px] items-center justify-between border-b-[1px] bg-white-base px-[8px] py-3 md:px-[24px] md:px-[8px]">
+    <div className="z-40 flex h-[72px] items-center justify-between border-b-[1px] bg-white-base px-[8px] py-3 md:px-[24px]">
       <SpSideBar getTaxons={getTaxons} isSignedIn={isSignedIn} />
-      <Link href="/" passHref>
+      <Link href="/" className="absolute left-[51px]" passHref>
         <Logo />
       </Link>
-      <div className="flex justify-center md:flex-1">
+      <div className="z-50 flex justify-center md:left-[34%] md:flex-1">
         <Search />
       </div>
-      <div className="flex items-center">
+      <div className="right-6 z-40 flex items-center justify-center gap-4 md:absolute">
         <AccountMenu isSignedIn={isSignedIn} getAccount={isSignedIn ? getAccount() : null} />
         <Suspense fallback={<LoadingSpinner />}>
           <CartMenu />
