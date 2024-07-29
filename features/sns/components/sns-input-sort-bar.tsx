7b3a9f41188090b9934efa4dc7f5140bf9ea5021
Tarek Aoukar <t.aoukar@octagrid.net>
2024-07-29 18:14:13 +0900

    add skin types and concerns to sns search bar

--- features/sns/components/sns-input-sort-bar.tsx
+++ features/sns/components/sns-input-sort-bar.tsx
@@ -1,15 +1,61 @@
 'use client';
 
-import { Command, CommandInput } from '@/components/ui/command';
+import { Button } from '@/components/ui/button';
+import { Command, CommandInput, CommandList } from '@/components/ui/command';
 import { Select, SelectContent, SelectItem, SelectTrigger } from '@/components/ui/select';
-import { useState } from 'react';
+import { Typography } from '@/components/ui/typography';
+import { useRef, useState } from 'react';
 
 interface SnsInputSortBarProps {
   onSortChange: (sortBy: 'followers_asc' | 'followers_desc') => void;
 }
 
+const skinTypes = ['普通肌', '乾燥肌', '脂性肌', '混合肌'];
+const skins = [
+  {
+    name: 'イエベ春タイプ',
+    color: '#F7D4C0'
+  },
+  {
+    name: 'ブルべ夏タイプ',
+    color: '#FFE0D8'
+  },
+  {
+    name: 'イエベ秋タイプ',
+    color: '#FCE6CE'
+  },
+  {
+    name: 'ブルべ冬タイプ',
+    color: '#FDEFEC'
+  }
+];
+const skinConcerns = [
+  'アトピー',
+  'ニキビ',
+  '敏感肌',
+  '美白/シミ',
+  '皮脂/ブラックヘッド',
+  'クマ',
+  '乾燥肌',
+  'シワ/弾力',
+  '毛穴',
+  '赤み',
+  '角質',
+  '該当なし'
+];
+
 export function SnsInputSortBar({ onSortChange }: SnsInputSortBarProps) {
   const [sortOption, setSortOption] = useState('フォロワー数(昇順)');
+  const [selectionVisible, setSelectionVisible] = useState(false);
+  const [selectedSkinType, setSelectedSkinType] = useState<string | undefined>(undefined);
+  const [selectedSkinColor, setSelectedSkinColor] = useState<string | undefined>(undefined);
+  const [selectedSkinConcerns, setSelectedSkinConcerns] = useState<string[]>([]);
+  const [closeTimeout, setCloseTimeout] = useState<ReturnType<typeof setTimeout> | undefined>(
+    undefined
+  );
+  const ref = useRef<HTMLInputElement>(null);
+
+  const hasInput = selectedSkinType || selectedSkinColor || selectedSkinConcerns.length;
 
   const handleSortChange = (value: string) => {
     const newSortOption = value === '1' ? 'フォロワー数(昇順)' : 'フォロワー数(降順)';
@@ -21,9 +67,124 @@ export function SnsInputSortBar({ onSortChange }: SnsInputSortBarProps) {
     <div className="mt-[24px] flex w-full flex-col items-center justify-center px-[8px] md:flex-row md:justify-between">
       <Command className="w-full bg-paleFrostBlue">
         <CommandInput
-          placeholder="例：普通肌・ニキビ・毛穴"
+          ref={ref}
+          placeholder={hasInput ? undefined : '例：普通肌・ニキビ・毛穴'}
           className="rounded-[44px] border-2 border-bibinBlue-100 bg-white-base md:w-2/3"
+          onFocus={() => {
+            clearTimeout(closeTimeout);
+            setSelectionVisible(true);
+          }}
+          onBlur={() => {
+            console.log('onBlur');
+            setCloseTimeout(
+              setTimeout(async () => {
+                setSelectionVisible(false);
+              }, 150)
+            );
+          }}
+          children={
+            hasInput ? (
+              <div className="flex h-full items-center whitespace-nowrap">
+                {selectedSkinType && (
+                  <Pill text={selectedSkinType} onRemove={() => setSelectedSkinType(undefined)} />
+                )}
+                {selectedSkinColor && (
+                  <Pill text={selectedSkinColor} onRemove={() => setSelectedSkinColor(undefined)} />
+                )}
+                {selectedSkinConcerns.map((concern, index) => (
+                  <Pill
+                    key={index}
+                    text={concern}
+                    onRemove={() =>
+                      setSelectedSkinConcerns(
+                        selectedSkinConcerns.filter((item) => item !== concern)
+                      )
+                    }
+                  />
+                ))}
+              </div>
+            ) : undefined
+          }
         />
+        <div>
+          <CommandList
+            className={
+              'absolute left-0 w-full rounded-md bg-white-base px-4 pt-4 md:left-auto md:max-w-[52%] md:border-2 ' +
+              (selectionVisible ? '' : ' hidden ')
+            }
+          >
+            <Typography className="p-[8px]" element="h3" as="boldXLarge">
+              肌質で検索
+            </Typography>
+            <Typography className="p-[8px]" element="p" as="boldSmall">
+              肌タイプ
+            </Typography>
+            <div className="grid grid-cols-2 md:grid-cols-4">
+              {skinTypes.map((type, index) => (
+                <Button
+                  key={index}
+                  variant={selectedSkinType == type ? 'secondary' : 'outline'}
+                  className={
+                    'm-1 border-2 p-6' + (selectedSkinType == type ? ' border-bibinBlue-100 ' : '')
+                  }
+                  onClick={(e) => {
+                    clearTimeout(closeTimeout);
+                    ref.current?.focus();
+                    setSelectedSkinType(selectedSkinType == type ? undefined : type);
+                  }}
+                >
+                  {type}
+                </Button>
+              ))}
+            </div>
+            <Typography className="p-[8px]" element="p" as="boldSmall">
+              パーソナルカラー
+            </Typography>
+            <div className="grid grid-cols-2 md:grid-cols-4">
+              {skins.map((type, index) => (
+                <SkinButton
+                  key={index}
+                  name={type.name}
+                  color={type.color}
+                  active={selectedSkinType == type.name}
+                  onClick={() => {
+                    clearTimeout(closeTimeout);
+                    ref.current?.focus();
+                    setSelectedSkinType(selectedSkinType == type.name ? undefined : type.name);
+                  }}
+                />
+              ))}
+            </div>
+            <Typography className="p-[8px]" element="p" as="boldSmall">
+              肌タイプ
+            </Typography>
+            <div className="grid grid-cols-2 md:grid-cols-4">
+              {skinConcerns.map((concern, index) => (
+                <Button
+                  key={index}
+                  variant={selectedSkinConcerns.includes(concern) ? 'secondary' : 'outline'}
+                  className={
+                    'm-1 border-2 p-6' +
+                    (selectedSkinConcerns.includes(concern) ? ' border-bibinBlue-100 ' : '')
+                  }
+                  onClick={(e) => {
+                    clearTimeout(closeTimeout);
+                    ref.current?.focus();
+                    if (selectedSkinConcerns.includes(concern)) {
+                      setSelectedSkinConcerns(
+                        selectedSkinConcerns.filter((item) => item !== concern)
+                      );
+                    } else {
+                      setSelectedSkinConcerns([...selectedSkinConcerns, concern]);
+                    }
+                  }}
+                >
+                  {concern}
+                </Button>
+              ))}
+            </div>
+          </CommandList>
+        </div>
       </Command>
       <Select onValueChange={handleSortChange}>
         <SelectTrigger className="mt-[8px] h-[48px] w-[300px] rounded-full border-[1px] border-bibinBlue-100 bg-paleFrostBlue px-[24px] py-[4px] text-[14px] font-semibold text-bibinBlue-100 md:mt-0">
@@ -35,5 +196,69 @@ export function SnsInputSortBar({ onSortChange }: SnsInputSortBarProps) {
         </SelectContent>
       </Select>
     </div>
+  );
+}
+
+type PillParams = {
+  text: string;
+  onRemove: () => void;
+};
+function Pill({ text, onRemove }: PillParams) {
+  return (
+    <Typography
+      className="mx-1 flex h-8 items-center rounded-full bg-[#D9F0FF] pl-2"
+      element="div"
+      as="xSmall"
+    >
+      {text}
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        onClick={onRemove}
+        style={{ cursor: 'pointer' }}
+      >
+        <path
+          d="M12 2C6.47 2 2 6.47 2 12C2 17.53 6.47 22 12 22C17.53 22 22 17.53 22 12C22 6.47 17.53 2 12 2ZM17 15.59L15.59 17L12 13.41L8.41 17L7 15.59L10.59 12L7 8.41L8.41 7L12 10.59L15.59 7L17 8.41L13.41 12L17 15.59Z"
+          fill="#51B7FF"
+        />
+      </svg>
+    </Typography>
+  );
+}
+
+type SkinParams = {
+  name: string;
+  color: string;
+  active: boolean;
+  onClick: () => void;
+};
+function SkinButton({ name, color, active, onClick }: SkinParams) {
+  return (
+    <Button
+      variant={active ? 'secondary' : 'outline'}
+      className={'m-1 h-fit border-2 p-3 ' + (active ? ' border-bibinBlue-100 ' : '')}
+      onClick={onClick}
+    >
+      <input type="radio" name="skinType" readOnly checked={active} className="mx-2" />
+      <div className="flex flex-col items-center">
+        <div
+          className="rounded-full"
+          style={{
+            boxSizing: 'border-box',
+            width: '32px',
+            height: '32px',
+            backgroundColor: color,
+            border: '1px solid rgba(0, 0, 0, 0.1)',
+            order: 0,
+            flex: 'none',
+            flexGrow: 0
+          }}
+        ></div>
+        {name}
+      </div>
+    </Button>
   );
 }
