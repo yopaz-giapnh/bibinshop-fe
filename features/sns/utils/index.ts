c76778bb20a2ee10ad890d731b99d67b61b3ea56
jin-331 <luckina.331@gmail.com>
2024-08-01 13:31:07 +0900

    fix prettier

--- features/sns/utils/index.ts
+++ features/sns/utils/index.ts
@@ -5,10 +5,9 @@ export function isUserProfile(item: unknown): item is components['schemas']['Use
   return hasProperty(item, 'type') && item.type === 'user_profile';
 }
 type UserProfile = components['schemas']['UserProfile'];
-type UserAttributes = UserProfile['attributes'];
-type ForcedAttributes = UserAttributes & {};
-export type SkinType = ForcedAttributes['skin_type'];
-export type PersonalColor = ForcedAttributes['personal_color'];
-export type SkinConcern = NonNullable<ForcedAttributes['skin_concerns']>;
-export type HairConcern = NonNullable<ForcedAttributes['scalp_hair_concerns']>;
-export type HealthConcern = NonNullable<ForcedAttributes['health_concerns']>;
+type UserAttributes = UserProfile['attributes'] & Record<string, never>;
+export type SkinType = UserAttributes['skin_type'];
+export type PersonalColor = UserAttributes['personal_color'];
+export type SkinConcern = NonNullable<UserAttributes['skin_concerns']>;
+export type HairConcern = NonNullable<UserAttributes['scalp_hair_concerns']>;
+export type HealthConcern = NonNullable<UserAttributes['health_concerns']>;
