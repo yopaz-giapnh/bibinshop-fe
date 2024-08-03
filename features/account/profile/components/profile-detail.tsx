060c77636487e19258478156c19171daae6cea96
jin-331 <luckina.331@gmail.com>
2024-08-03 17:20:44 +0900

    fix recieveFeedbackReviewsCount

--- features/account/profile/components/profile-detail.tsx
+++ features/account/profile/components/profile-detail.tsx
@@ -64,7 +64,7 @@ export default async function ProfileDetail({ isSpHomeProfile = false }: Props) 
             <UserDetailProfileStats
               reviewsCount={reviewsCount}
               followersCount={followersCount}
-              receivedFeedbackReviewsCount={4}
+              receivedFeedbackReviewsCount={receivedFeedbackReviewsCount}
               followeesCount={followeesCount}
               uniqueKey={userUniqueKey}
               tags={tags}
