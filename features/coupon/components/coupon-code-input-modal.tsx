ae4fee24dace89b43216c3d92dc4850109f4601b
前田正勝 <katsukatsu.masa.0522@gmail.com>
2024-08-20 12:48:02 +0900

    クーポン入力モーダルUI fix

---
+++ features/coupon/components/coupon-code-input-modal.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogDescription } from '@/components/ui/dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Typography } from '@/components/ui/typography';
+import { useToast } from '@/components/ui/use-toast';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Check } from 'lucide-react';
+import { forwardRef, useImperativeHandle, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+export type CouponCodeInputModalRef = {
+  open: () => void;
+  close: () => void;
+};
+
+const formSchema = z.object({
+  couponCode: z.string().min(1, { message: 'クーポンコードが正しくありません' })
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+export const CouponCodeInputModal = forwardRef<CouponCodeInputModalRef>((_, ref) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { toast } = useToast();
+
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      couponCode: ''
+    }
+  });
+
+  useImperativeHandle(ref, () => ({
+    open: () => setIsOpen(true),
+    close: () => setIsOpen(false)
+  }));
+
+  const onSubmit = async (values: FormValues) => {
+    // TODO: ここでクーポンコードの検証や適用のロジックを実装
+    console.log(values);
+    toast({
+      title: 'クーポンが追加されました',
+      icon: <Check className="h-6 w-6" />
+    });
+    setIsOpen(false);
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogDescription>
+        <DialogContent className="flex flex-col items-center">
+          <Typography as="bold" element="h2" className="mb-4 text-center text-lg">
+            クーポンコードを入力
+          </Typography>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full items-center">
+              <FormField
+                control={form.control}
+                name="couponCode"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>クーポンコードを入力してください</FormLabel>
+                    <div className="flex items-center">
+                      <FormControl>
+                        <Input {...field} placeholder="クーポンコード" />
+                      </FormControl>
+                      <Button
+                        type="submit"
+                        className="ml-2 px-8 py-2 font-semibold"
+                        disabled={!form.watch('couponCode')}
+                      >
+                        追加
+                      </Button>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+          <button
+            type="button"
+            onClick={() => setIsOpen(false)}
+            className="mt-2 w-full rounded-full border-[1px] border-bibinBlue-100 px-12 py-2 font-semibold text-bibinBlue-100 md:mt-4 md:w-fit md:px-16 md:py-4"
+          >
+            キャンセル
+          </button>
+        </DialogContent>
+      </DialogDescription>
+    </Dialog>
+  );
+});
+
+CouponCodeInputModal.displayName = 'CouponCodeInputModal';
--- features/coupon/components/coupon.tsx
+++ features/coupon/components/coupon.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { BackButton } from '@/components/button/back-button';
+import { Pagination } from '@/components/ui/pagination';
 import { Typography } from '@/components/ui/typography';
+import { useRef } from 'react';
 import { CouponCard } from './coupon-card';
+import { CouponCodeInputModal, CouponCodeInputModalRef } from './coupon-code-input-modal';
 import CouponListEmptyView from './coupon-list-empty-view';
 
 // クーポン情報のdemo型定義
@@ -63,6 +66,8 @@ const coupons: CouponInfo[] = [
 ];
 
 export default function Coupon() {
+  const couponCodeInputModalRef = useRef<CouponCodeInputModalRef>(null);
+
   return (
     <div className="mx-auto flex w-full flex-col items-center bg-paleFrostBlue p-[16px] md:p-[24px]">
       <div className="mb-[14px] flex w-full items-center justify-between md:mb-[24px] md:justify-center">
@@ -96,14 +101,20 @@ export default function Coupon() {
             </div>
           </div>
         )}
+        {/* TODO:ページング */}
+        <Pagination />
         <div className="mt-[24px] flex w-full flex-col items-center justify-center rounded-md bg-white-base py-[24px] shadow-sm">
           <Typography as="bold" element="p" className="text-[16px] text-black-90 md:text-[20px]">
             クーポンが見つかりませんか？
           </Typography>
-          <button className="mt-[16px] w-11/12 rounded-full border-[1px] border-bibinBlue-100 px-[48px] py-[8px] text-[14px] font-semibold text-bibinBlue-100 md:w-1/3">
+          <button
+            className="mt-[16px] w-11/12 rounded-full border-[1px] border-bibinBlue-100 px-[48px] py-[8px] text-[14px] font-semibold text-bibinBlue-100 md:w-1/3"
+            onClick={() => couponCodeInputModalRef.current?.open()}
+          >
             クーポンコードを入力する
           </button>
         </div>
+        <CouponCodeInputModal ref={couponCodeInputModalRef} />
       </div>
     </div>
   );
