9bdac910aa8c2a17c83207b7f7e5e707118797f5
Tarek Aoukar <t.aoukar@octagrid.net>
2024-08-24 21:29:22 +0900

    require old password when changing password.

--- features/account/security/actions/index.ts
+++ features/account/security/actions/index.ts
@@ -14,7 +14,7 @@ export async function updateAccountSecurity(prevState: State | null, formData: F
     const { error } = await apiClient.PATCH('/api/v2/storefront/account', {
       body: {
         user: {
-          // TODO:BE側のRequest body古いパスワード追加後に追記
+          current_password: formData.password,
           password: formData.newPassword,
           password_confirmation: formData.newConfirmPassword
         }
