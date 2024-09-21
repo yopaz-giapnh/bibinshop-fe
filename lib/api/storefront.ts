071be6fe0d7cfefb1188e8ab34f02e335f1ce1ee
jin-331 <luckina.331@gmail.com>
2024-09-21 20:41:18 +0900

    generate:type;

--- lib/api/storefront.ts
+++ lib/api/storefront.ts
@@ -5817,11 +5817,7 @@ export interface operations {
       200: {
         content: {
           'application/vnd.api+json': {
-<<<<<<< HEAD
             data?: components['schemas']['Cart'];
-=======
-            data?: components['schemas']['Order'];
->>>>>>> 36e4073 (generate type)
           };
         };
       };
