659461a536f70cede22463b82c369ad4efe82a67
前田正勝 <katsukatsu.masa.0522@gmail.com>
2024-10-07 15:17:02 +0900

    検索バーUI修正

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -27,9 +27,9 @@ export function Search({ isSignedIn }: Props) {
     <Command
       className={
         (isOpen
-          ? 'absolute left-0 top-0 z-50 h-screen w-screen pt-[15px] md:h-fit md:max-h-[300px] md:w-[462px] md:pt-[89px]'
+          ? 'absolute left-0 top-0 z-50 h-screen w-screen pt-[15px] md:h-fit md:max-h-[300px] md:w-[462px] md:pt-[0px]'
           : `absolute ${isSignedIn ? 'top-3' : 'top-9'} z-50 h-[48px] w-[172px]`) +
-        ' md:static md:left-auto md:top-auto md:flex md:w-[456px]'
+        ' md:left-auto md:top-3 md:flex md:w-[456px]'
       }
       value={searchValue}
     >
