565be012611aad2ddea2ce23ecc7f45264020f79
前田正勝 <katsukatsu.masa.0522@gmail.com>
2024-10-09 12:30:12 +0900

    商品price表示修正

--- features/product/components/product-card.tsx
+++ features/product/components/product-card.tsx
@@ -101,28 +101,30 @@ export function ProductCard({ product, imageSize, deleteButtonAction = undefined
         </Typography>
 
         <div className="mt-[4px] flex w-full items-end justify-between">
-          <Typography as="bold" element="p" className="text-bibinBlue-100">
-            {formatedPrice(product.attributes.price)}
-          </Typography>
+          <div className="flex items-end">
+            <Typography as="bold" element="p" className="text-bibinBlue-100">
+              {formatedPrice(product.attributes.price)}
+            </Typography>
 
-          {isDiscounted(product.attributes.price, product.attributes.compare_at_price) && (
-            <>
-              <div className="relative flex items-center justify-center">
-                <Typography as="small" element="p" className="text-black-20">
-                  {formatedPrice(product.attributes.compare_at_price)}
-                </Typography>
-                <div className="absolute h-[1px] w-full bg-black-20" />
-              </div>
-              <div className="flex items-center rounded-[4px] border border-lightRed px-1 py-[0.5px]">
-                <Typography as="xSmall" element="p" className="text-lightRed">
-                  {`-${calculateDiscountPercentage(
-                    product.attributes.price,
-                    product.attributes.compare_at_price
-                  )}%`}
-                </Typography>
-              </div>
-            </>
-          )}
+            {isDiscounted(product.attributes.price, product.attributes.compare_at_price) && (
+              <>
+                <div className="relative ml-2 flex items-center justify-center">
+                  <Typography as="small" element="p" className="text-black-20">
+                    {formatedPrice(product.attributes.compare_at_price)}
+                  </Typography>
+                  <div className="absolute h-[1px] w-full bg-black-20" />
+                </div>
+                <div className="ml-2 flex items-center rounded-[4px] border border-lightRed px-1 py-[0.5px]">
+                  <Typography as="xSmall" element="p" className="text-lightRed">
+                    {`-${calculateDiscountPercentage(
+                      product.attributes.price,
+                      product.attributes.compare_at_price
+                    )}%`}
+                  </Typography>
+                </div>
+              </>
+            )}
+          </div>
           <Typography
             as="xSmall"
             element="p"
