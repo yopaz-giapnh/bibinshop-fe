f044290be827dd3acd1ff1b6a084afb015c896cc
前田正勝 <katsukatsu.masa.0522@gmail.com>
2024-11-25 18:01:55 +0900

    fix purchase products and reviews pagination

--- features/sns/components/user-detail.tsx
+++ features/sns/components/user-detail.tsx
@@ -2,11 +2,22 @@ import { User } from '@/features/users/types';
 import UserDetailProfile from './user-detail-profile';
 import { UserDetailTabs } from './user-detail-tabs';
 
-export async function UserDetail({ userDetail }: { userDetail: User }) {
+type Props = {
+  userDetail: User;
+  searchParams?: {
+    state?: string;
+    page?: string;
+  };
+};
+
+export async function UserDetail({ userDetail, searchParams }: Props) {
+  const tabState = searchParams?.state || 'review';
+  const currentPage = Number(searchParams?.page) || 1;
+
   return (
     <div className="mx-auto flex h-full w-full flex-col items-center bg-paleFrostBlue pb-[48px] pt-[73px] md:px-32 md:pt-[128px]">
       <UserDetailProfile userDetail={userDetail} />
-      <UserDetailTabs tabState="review" currentPage={1} userDetail={userDetail} />
+      <UserDetailTabs tabState={tabState} currentPage={currentPage} userDetail={userDetail} />
     </div>
   );
 }
