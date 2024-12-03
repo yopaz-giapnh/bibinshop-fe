8af8331fe480c180d8642f7e64b2bf0390f4205a
前田正勝 <katsukatsu.masa.0522@gmail.com>
2024-12-04 01:39:56 +0900

    create terms of use page

--- features/auth/constants/index.ts
+++ features/auth/constants/index.ts
@@ -1,5 +1,5 @@
 export const ROUTES = {
   // TODO: 一旦仮置き。必要であれば正しいものに置き換える
   PRIVACY_POLICY: 'https://bibin.jp/privacy',
-  TERMS_OF_SERVICE: '/terms-of-service'
+  TERMS_OF_USE: '/terms-of-use'
 };
