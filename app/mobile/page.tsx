003006cf202843b9aaabee593e1d96bfdd6c097a
Eiji <50960409+justkidding55boy@users.noreply.github.com>
2024-12-18 15:04:45 +0900

    Refactor redirect logic in mobile page and ensure consistent string formatting
    - Updated string quotes in `page.tsx` for consistency.
    - Improved redirect logic to handle confirmation tokens more clearly.
    - Ensured no newline at the end of the file in `index.ts` for cleaner formatting.

--- app/mobile/page.tsx
+++ app/mobile/page.tsx
@@ -1,16 +1,16 @@
-"use client";
-import { MOBILE_SCHEME, REDIRECT_TO_SIGNUP_PATH } from "@/features/auth/constants";
-import { redirect, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+'use client';
+import { MOBILE_SCHEME, REDIRECT_TO_SIGNUP_PATH } from '@/features/auth/constants';
+import { redirect, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function RedirectToSignup() {
   const searchParams = useSearchParams();
-  const confirmationToken = searchParams.get("confirmation_token");
+  const confirmationToken = searchParams.get('confirmation_token');
 
   useEffect(() => {
     if (confirmationToken) {
       window.location.href = `${MOBILE_SCHEME}${REDIRECT_TO_SIGNUP_PATH}?confirmation_token=${confirmationToken}`;
-      redirect("/");
+      redirect('/');
     }
   }, [confirmationToken]);
 
