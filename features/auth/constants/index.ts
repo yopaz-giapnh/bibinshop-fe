8b43da1be495d60c5d904d186218fa1b89ad96d7
Eiji <50960409+justkidding55boy@users.noreply.github.com>
2024-12-18 14:55:40 +0900

    Add mobile scheme and redirect path for signup

--- features/auth/constants/index.ts
+++ features/auth/constants/index.ts
@@ -2,3 +2,7 @@ export const ROUTES = {
   PRIVACY_POLICY: '/privacy-policy',
   TERMS_OF_USE: '/terms-of-use'
 };
+
+export const MOBILE_SCHEME = 'exp+bibin-shop-native://';
+
+export const REDIRECT_TO_SIGNUP_PATH = 'signup';
