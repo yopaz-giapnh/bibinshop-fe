19ffe9862a777ba617bbae1463750d50429fd016
前田正勝 <katsukatsu.masa.0522@gmail.com>
2025-01-20 15:59:34 +0900

    fix

--- app/account/message/page.tsx
+++ app/account/message/page.tsx
@@ -1,5 +1,6 @@
 import { BackButton } from '@/components/button/back-button';
 import { Typography } from '@/components/ui/typography';
+import { AnimatedMessageContainer } from '@/features/account/message/components/animated-message-container';
 import MessageList from '@/features/account/message/components/message-list';
 import { MessageListSkeleton } from '@/features/account/message/components/skeletons/message-list-skeleton';
 import { Suspense } from 'react';
@@ -18,21 +19,23 @@ export default function Page({
   const currentPage = Number(searchParams?.page) || 1;
 
   return (
-    <div className="mx-auto mb-[200px] flex w-full flex-col items-center bg-paleFrostBlue p-[16px] md:mb-0 md:h-screen md:p-[24px]">
-      <div className="flex w-full items-center justify-between pb-[24px] md:justify-center">
-        <BackButton />
-        <Typography
-          as="boldXLarge"
-          element="p"
-          className="text-[16px] text-black-90 md:text-[24px]"
-        >
-          メッセージ
-        </Typography>
-        <div className="h-7 w-7" />
+    <AnimatedMessageContainer>
+      <div className="mx-auto mb-[200px] flex w-full flex-col items-center bg-paleFrostBlue p-[16px] md:mb-0 md:h-screen md:p-[24px]">
+        <div className="flex w-full items-center justify-between pb-[24px] md:justify-center">
+          <BackButton />
+          <Typography
+            as="boldXLarge"
+            element="p"
+            className="text-[16px] text-black-90 md:text-[24px]"
+          >
+            メッセージ
+          </Typography>
+          <div className="h-7 w-7" />
+        </div>
+        <Suspense fallback={<MessageListSkeleton />}>
+          <MessageList currentPage={currentPage} />
+        </Suspense>
       </div>
-      <Suspense fallback={<MessageListSkeleton />}>
-        <MessageList currentPage={currentPage} />
-      </Suspense>
-    </div>
+    </AnimatedMessageContainer>
   );
 }
