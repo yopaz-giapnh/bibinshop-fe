803da0c59ec2940fbcdfc8d17395be40addd0c48
前田正勝 <katsukatsu.masa.0522@gmail.com>
2025-02-04 18:52:32 +0900

    fix

--- components/layout/footer/menu.tsx
+++ components/layout/footer/menu.tsx
@@ -7,9 +7,10 @@ import Link from 'next/link';
 export function Menu() {
   return (
     <div className="flex h-[114px] w-full items-center justify-center gap-5 bg-bibinBlue-100 pl-[16px] md:h-[124px] md:gap-16">
-      <MenuItem href="/todo" icon={<Shipping />} label="配送について" />
+      {/* TODO: リンク先を設定する */}
+      <MenuItem href="/" icon={<Shipping />} label="配送について" />
       <Separator />
-      <MenuItem href="/todo" icon={<Question />} label="サポート＆FAQ" />
+      <MenuItem href="/" icon={<Question />} label="サポート＆FAQ" />
       <Separator />
       <MenuItem href="/signup" icon={<User />} label="メンバー登録" />
     </div>
