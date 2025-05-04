ced0e293e75ce5794cf3b6480e5e126d66fb66dc
masa <65245621+masa2205@users.noreply.github.com>
2025-05-04 11:20:40 +0900

    Revert "検索エンジンからのウェブサイトの非公開化対応"

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({ children }: PropsWithChildren) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
-        <meta name="robots" content="noindex" />
         <meta name="google-site-verification" content={META.googleSiteVerification} />
       </head>
       <body
